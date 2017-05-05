55a1b212c2735eb1122943cecbe780866ea48568
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2017-05-05 16:36:41 -0400

    Moved functions to new file.

---
+++ signalerlib.c
@@ -0,0 +1,57 @@
+#include <string.h>
+#include <signal.h>
+#include "signalerlib.h"
+
+extern unsigned int num = 3;
+extern int reverse_flag = 0;
+extern int skip_flag = 0;
+extern int exit_flag = 0;
+
+unsigned int handle_args(int argc, char * argv[], unsigned int end)
+{
+    if (argc < 2) {
+        return(2);
+    }
+    for(int i = 0; i < argc; ++i) {
+
+        char * cur = argv[i];
+
+        if (strncmp(cur, "-r", 2) == 0) {
+            reverse_flag = 1;
+        }
+
+        if (strncmp(cur, "-s", 2) == 0){
+            unsigned int start = strtol(argv[i + 1], NULL, 10);
+            if (start >= 0) {
+                num = start;
+            }
+        }
+
+        if (strncmp(cur, "-e", 2) == 0) {
+            end = strtol(argv[i + 1], NULL, 10);
+            if (end >= 0) {
+                exit_flag = 1;
+            }
+        }
+    }
+    return(end);
+}
+
+void handler (int sig_num)
+{
+    if (sig_num == SIGHUP) {
+        num = 3;
+        reverse_flag = 0;
+    }
+
+    if (sig_num == SIGUSR1) {
+        skip_flag = 1;
+    }
+    if (sig_num == SIGUSR2) {
+        if (reverse_flag == 0) {
+            reverse_flag = 1;
+        } else {
+            reverse_flag = 0;
+        }
+    }
+}
---
+++ signalerlib.h
@@ -0,0 +1,10 @@
+#ifndef SIGNALERLIB_H
+#define SIGNALERLIB_H
+
+
+
+unsigned int handle_args(int argc, char * argv[], unsigned int end);
+
+void handler (int sig_num);
+
+#endif
